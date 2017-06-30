e67821b589feb00df66229abbc49ecf1e790cc8e
Eric Andersson <eaherrgarden@gmail.com>
2017-06-30 09:31:24 +0200

    Added animate.py where I will start writing script for generating animations of data from the pencil-code.

---
+++ animate.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# Last Modification: 30-06-2017, Eric Andersson <eaherrgarden@gmail.com>
+#=======================================================================
+# animate.py
+#
+# Contains all animation functions used for adaptive particles.
+#
+# Eric Andersson, 30-06-2017
+#=======================================================================
