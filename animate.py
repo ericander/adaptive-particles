738ec8fa7526e2c48489a42ead2a4711af77b7de
Eric Andersson <eaherrgarden@gmail.com>
2017-07-06 14:05:54 +0200

    Added function for creating an animation that shows the cumulative denisty distribution changes in time. Works only for 2D data because it utilizes the pc.read.slices() function to optimize cadence between frames.

--- animate.py
+++ animate.py
@@ -40,7 +40,6 @@ def grid_density_1D(filename, dim, animdir = './animations/',
     import numpy as np
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
-    import sys, os
 
     # Parameters used in run
     print("Setting up parameters...")
@@ -123,3 +122,114 @@ def grid_density_1D(filename, dim, animdir = './animations/',
     print("Done.")
     plt.show()
 #=======================================================================
+
+def cumulative_rhop_2D(animdir = './animations/', xlim = None, ylim = None, xlog = True, ylog = True, filename = 'animation', fps = 24):
+    """ Plots the cumulative particle density distribution.
+
+    Keyword Argument:
+        animdir
+            Directory for saving animation.
+        xlim
+            If limits is of type tuple it will set the x-axis limit
+            to the given values.
+        ylim
+            If limits is of type tuple it will set the y-axis limit
+            to the given values.
+        xlog
+            True/False whether to use log scale on x-axis or not.
+        ylog
+            True/False whether to use log scale on y-axis or not.
+        filename
+            Name of output file.
+        fps
+            Frames per second in animation
+
+    """
+    # Eric Andersson, 06-07-2017
+    import PencilCode as pc
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    ### WARNING ###
+    # ONLY WORKS IN 2D SIMULATIONS!
+    dim = pc.read.dimensions()
+    ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
+    if ndim == 3:
+        raise NotImplementedError("More than 2 dimensions is not implemented yet")
+
+    # Import and set-up data from the pencil-code
+    print("Reading and setting up data")
+    t, rhop = pc.read.slices('rhop')
+    plane = rhop.dtype.names
+    for p in plane:
+        if (rhop[0][p].shape[0] != 1) and (rhop[0][p].shape[1] != 1):
+            plane = p
+            break
+    print("Done.")
+    print("Working along the " + plane + "-plane")
+
+
+    # Parameters used in run
+    print("Setting up parameters...")
+    param = pc.read.parameters()
+    taus = param.taus               # Dimensionless stopping time
+    eps = param.eps_dtog            # Local mass density ratio
+    print("Done.")
+
+    # Set up figure
+    print("Setting up figure...")
+    fig  = plt.figure()
+    ax = fig.add_subplot(111)
+    line, = ax.step([], [],
+            label = r'$\tau_s = {},\ \epsilon = {}$'.format(
+                taus, eps))
+    plt.minorticks_on()
+    plt.xlabel(r'$\rho_p/<\rho_p>$')
+    plt.ylabel(r'$P(<\rho_p)$')
+    plt.legend(loc = 'lower left')
+
+    # Set up time text
+    time_text = ax.text(0.7, 0.95, '', transform=ax.transAxes)
+
+    # Set axis scales
+    if xlog and ylog:
+        plt.xscale('log')
+        plt.yscale('log')
+    elif xlog:
+        plt.xscale('log')
+    elif ylog:
+        plt.yscale('log')
+
+    # Set axis limits
+    if type(xlim) is tuple:
+        plt.xlim(xlim)
+    if type(ylim) is tuple:
+        plt.ylim(ylim)
+
+    print("Done.")
+
+    # Function that sets up background of each frame
+    def init():
+        line.set_data([], [])
+        time_text.set_text('')
+        return line, time_text
+
+
+    # Function that is called everytime a new frame is produced
+    def animate(i):
+        print("\rAnimating ({:6.1%})......".format(i/t.size),
+                end='', flush=True)
+        time_text.set_text(r'Time = {0:.2f} $\Omega t$'.format(t[i]))
+        rhopcum = np.sort(np.concatenate(rhop[i][plane]))
+        avgrhop = np.mean(rhopcum)
+        line.set_data(rhopcum[::-1]/avgrhop,
+            np.arange(rhopcum.size)/rhopcum.size)
+        return line, time_text
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                frames=t.size, interval=200, blit=False, repeat = False)
+    anim.save(filename + '.mp4', fps = fps,
+            extra_args=['-vcodec', 'libx264'])
+    print("Done.")
+    plt.show()
