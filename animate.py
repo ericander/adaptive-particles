e21d133a14e1095041e8b780ae854132fc382361
Eric Andersson <eaherrgarden@gmail.com>
2017-07-07 11:18:43 +0200

    Added function for animating histogram of particle density in given cells. Works only in 2D simulations.

--- animate.py
+++ animate.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# Last Modification: 04-07-2017, Eric Andersson <eaherrgarden@gmail.com>
+# Last Modification: 07-07-2017, Eric Andersson <eaherrgarden@gmail.com>
 #=======================================================================
 # animate.py
 #
@@ -156,7 +156,8 @@ def cumulative_rhop_2D(animdir = './animations/', xlim = None, ylim = None, xlog
     dim = pc.read.dimensions()
     ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
     if ndim == 3:
-        raise NotImplementedError("More than 2 dimensions is not implemented yet")
+        raise NotImplementedError(
+                "More than 2 dimensions is not implemented yet")
 
     # Import and set-up data from the pencil-code
     print("Reading and setting up data")
@@ -228,8 +229,158 @@ def cumulative_rhop_2D(animdir = './animations/', xlim = None, ylim = None, xlog
         return line, time_text
 
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                frames=t.size, interval=200, blit=False, repeat = False)
-    anim.save(filename + '.mp4', fps = fps,
+                                frames=t.size, interval=200,
+                                blit=False, repeat = False)
+    anim.save(animdir + filename + '.mp4', fps = fps,
             extra_args=['-vcodec', 'libx264'])
     print("Done.")
     plt.show()
+
+#======================================================================
+def rhop_histogram_2D(animdir = './animations/', xlim = (0.01, 10),
+        ylim = (0, 600), xlog = False, ylog = False,
+        filename = 'animation', fps = 24):
+    """ Plots the cumulative particle density distribution.
+
+    Keyword Argument:
+        animdir
+            Directory for saving animation.
+        xlim
+            If limits is of type tuple it will set the x-axis limit
+            to the given values.
+        ylim
+            If limits is of type tuple it will set the y-axis limit
+            to the given values.
+        xlog
+            True/False whether to use log scale on x-axis or not.
+        ylog
+            True/False whether to use log scale on y-axis or not.
+        filename
+            Name of output file.
+        fps
+            Frames per second in animation
+
+    """
+    # Eric Andersson, 06-07-2017
+    import PencilCode as pc
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    ### WARNING ###
+    # ONLY WORKS IN 2D SIMULATIONS!
+    dim = pc.read.dimensions()
+    ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
+    if ndim == 3:
+        raise NotImplementedError(
+                "More than 2 dimensions is not implemented yet")
+
+    # Import and set-up data from the pencil-code
+    print("Reading and setting up data")
+    t, rhop = pc.read.slices('rhop')
+    plane = rhop.dtype.names
+    for p in plane:
+        if (rhop[0][p].shape[0] != 1) and (rhop[0][p].shape[1] != 1):
+            plane = p
+            break
+    print("Done.")
+    print("Working along the " + plane + "-plane")
+
+
+    # Parameters used in run
+    print("Setting up parameters...")
+    param = pc.read.parameters()
+    taus = param.taus               # Dimensionless stopping time
+    eps = param.eps_dtog            # Local mass density ratio
+    print("Done.")
+
+    # Set up figure
+    print("Setting up figure...")
+    fig  = plt.figure()
+    ax = fig.add_subplot(111)
+    print("Done.")
+
+    # Function that sets up background of each frame
+    def init():
+        ax.text(0.75, 0.85, r'$\tau_s = {},\ \epsilon = {}$'.format(
+                taus, eps), transform=ax.transAxes)
+        plt.minorticks_on()
+        plt.xlabel(r'$\rho_p/<\rho_p>$')
+        plt.ylabel(r'$N_p$')
+        if type(ylim) is tuple:
+            plt.ylim(ylim)
+        # Set axis scales
+        if xlog and ylog:
+            plt.xscale('log')
+            plt.yscale('log')
+        elif xlog:
+            plt.xscale('log')
+        elif ylog:
+            plt.yscale('log')
+        # Set up time text
+        time_text = ax.text(0.75, 0.95,
+            r'Time = {0:.2f} $\Omega t$'.format(t[0]),
+            transform=ax.transAxes)
+        if type(xlim) is tuple:
+            if xlog:
+                bins = np.logspace(np.log10(xlim[0]),
+                                    np.log10(xlim[1]), 50)
+            else:
+                bins = np.linspace(xlim[0], xlim[1], 50)
+            ax.hist(np.concatenate(
+                rhop[0]['xz'])/np.mean(rhop[0][plane]),
+                bins = bins)
+        else:
+            ax.hist(np.concatenate(
+                rhop[0]['xz'])/np.mean(rhop[0][plane]),
+                bins = 'auto')
+
+    # Function that is called everytime a new frame is produced
+    def animate(i):
+        print("\rAnimating ({:6.1%})......".format(i/t.size),
+                end='', flush=True)
+        # Reset figure.
+        ax.clear()
+        plt.minorticks_on()
+        plt.xlabel(r'$\rho_p/<\rho_p>$')
+        plt.ylabel(r'$N_{\rm cells}$')
+        if type(ylim) is tuple:
+            plt.ylim(ylim)
+        # Set axis scales
+        if xlog and ylog:
+            plt.xscale('log')
+            plt.yscale('log')
+        elif xlog:
+            plt.xscale('log')
+        elif ylog:
+            plt.yscale('log')
+        # Plot data
+        ax.text(0.75, 0.85, r'$\tau_s = {},\ \epsilon = {}$'.format(
+                taus, eps), transform=ax.transAxes)
+        ax.text(0.75, 0.95,
+            r'Time = {0:.2f} $\Omega t$'.format(t[i]),
+            transform=ax.transAxes)
+        if type(xlim) is tuple:
+            if xlog:
+                bins = np.logspace(np.log10(xlim[0]),
+                                    np.log10(xlim[1]), 50)
+            else:
+                bins = np.linspace(xlim[0], xlim[1], 50)
+            ax.hist(np.concatenate(
+                rhop[i]['xz'])/np.mean(rhop[i][plane]),
+                bins = bins)
+        else:
+            ax.hist(np.concatenate(
+                rhop[i]['xz'])/np.mean(rhop[i][plane]),
+                bins = 'auto')
+
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                frames=t.size, interval=200,
+                                blit=False, repeat = False)
+    anim.save(animdir + filename + '.mp4', fps = fps,
+            extra_args=['-vcodec', 'libx264'])
+    print("Done.")
+    plt.show()
+
+#======================================================================
