238685d8bac644315be67940e1ca167c00d1fb8e
Eric Andersson <eaherrgarden@gmail.com>
2017-07-11 17:43:01 +0200

    Added function that plots cumulative denisty distribution averaged over time. The function works in a way similar to the histogram function.

--- plot.py
+++ plot.py
@@ -57,7 +57,7 @@ def rhopcumdist(plotdir = './plots/', xlim = False, ylim = False, xlog = True, y
             label = r"$\tau_s = 1.0$, $\epsilon = 0.2$")
     plt.minorticks_on()
     plt.xlabel(r"$\rho_p/<\rho_p>$")
-    plt.ylabel(r"P $(<\rho_p)$")
+    plt.ylabel(r"P$\ (<\rho_p)$")
     plt.legend(loc='best')
 
     # Save the plot
@@ -68,10 +68,10 @@ def rhopcumdist(plotdir = './plots/', xlim = False, ylim = False, xlog = True, y
 
 #======================================================================
 
-def time_averaged_density_histogram(t0, t,
+def cum_dens_dist_taverage(t0, t,
         datadir = './runs/', plotdir = './plots/',
-    xlim = (1e-4, 1e3), ylim = (0, 600), xlog = True, ylog = False,
-    filename = None, nbins = 50):
+    xlim = (1e-3, 1e3), ylim = (1e-5, 1e0), xlog = True, ylog = True,
+    filename = None):
     """ Plots the cumulative particle density distribution averaged
     over time to obtain a stable result.
 
@@ -82,6 +82,10 @@ def time_averaged_density_histogram(t0, t,
             Number of timesteps for average
 
     Keyword Arguments:
+        datadir
+            Directory of data files.
+        plotdir
+            Directory for saving plots
         xlim
             If limits is of type tuple it will set the x-axis limit
             to the given values.
@@ -102,6 +106,105 @@ def time_averaged_density_histogram(t0, t,
     import numpy as np
     import matplotlib.pyplot as plt
     import sys, os
+
+    # Set up axis
+    if xlog and ylog:
+        plt.xscale('log')
+        plt.yscale('log')
+    elif xlog:
+        plt.xscale('log')
+    elif ylog:
+        plt.yscale('log')
+    if type(xlim) is tuple:
+        plt.xlim(xlim)
+    if type(ylim) is tuple:
+        plt.ylim(ylim)
+
+    if datadir == './runs/':
+        runs = [line.rstrip('\n') for line in open(datadir + 'runs.txt')]
+    else:
+        print('Wrong directory.')
+        exit()
+
+    # Read data and create plot
+    for run in runs:
+        print('Plotting ' + run)
+        for i in range(t0, t0+t):
+            sys.stdout = open(os.devnull, 'w')
+            dim = pc.read.dimensions(datadir = datadir + run + '/data/')
+            ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
+            f = pc.read.var(datadir = datadir + run + '/data/',
+                                ivar = i)
+            sys.stdout = sys.__stdout__
+            rhop = np.zeros((t, dim.nxgrid*dim.nygrid*dim.nzgrid))
+            if ndim == 1:
+                rhop[i-t0][:] = np.sort(f.rhop)[::-1]
+            elif ndim == 2:
+                rhop[i-t0][:] = np.sort(
+                        np.concatenate(f.rhop))[::-1]
+            else:
+                rhop[i-t0][:] = np.sort(np.concatenate(np.concatenate(
+                                        f.rhop)))[::-1]
+        rhopcum = np.mean(rhop, axis=0)
+        param = pc.read.parameters(datadir = datadir + run + '/data/')
+        plt.step(rhopcum/np.mean(rhopcum),
+            np.arange(rhopcum.size)/rhopcum.size,
+            label = r'$\tau_s = {},\ \epsilon = {}$'.format(
+                param.taus, param.eps_dtog))
+
+    plt.minorticks_on()
+    plt.xlabel(r"$\rho_p/<\rho_p>$")
+    plt.ylabel(r"P$\ (<\rho_p)$")
+    plt.legend(loc='best')
+
+    # Save the plot
+    if type(filename) is str:
+        plt.savefig(plotdir + filename + '.pdf', bbox_inches='tight')
+
+    plt.show()
+
+
+
+#======================================================================
+
+def time_averaged_density_histogram(t0, t,
+        datadir = './runs/', plotdir = './plots/',
+    xlim = (1e-4, 1e3), ylim = (0, 600), xlog = True, ylog = False,
+    filename = None):
+    """ Plots the cumulative particle density distribution averaged
+    over time to obtain a stable result.
+
+    Positional Arguments:
+        t0
+            Initial timestep
+        t
+            Number of timesteps for average
+
+    Keyword Arguments:
+        datadir
+            Directory of data files.
+        plotdir
+            Directory for saving plots
+        xlim
+            If limits is of type tuple it will set the x-axis limit
+            to the given values.
+        ylim
+            If limits is of type tuple it will set the y-axis limit
+            to the given values.
+        xlog
+            True/False whether to use log scale on x-axis or not.
+        ylog
+            True/False whether to use log scale on y-axis or not.
+        filename
+            If filename is given as string it will save plot as
+            'filename.pdf'.
+    """
+    # Eric Andersson, 11-07-2017
+    import PencilCode as pc
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import sys, os
+
     # Set up axis
     if xlog and ylog:
         plt.xscale('log')
@@ -126,6 +229,8 @@ def time_averaged_density_histogram(t0, t,
         exit()
     # Allocate array for storing data
     rhop = np.zeros((t, 49))
+    stdrhop = {}
+    avgrhop = {}
     # Read data and create plot
     for run in runs:
         print('Plotting ' + run)
@@ -137,22 +242,24 @@ def time_averaged_density_histogram(t0, t,
                                 ivar = i)
             sys.stdout = sys.__stdout__
             if ndim == 1:
-                rhop[t0-i][:] = np.histogram(f.rhop/np.mean(f.rhop),
+                rhop[i-t0][:] = np.histogram(f.rhop/np.mean(f.rhop),
                                             bins = bins)[0]
             elif ndim == 2:
-                rhop[t0-i][:] = np.histogram(np.concatenate(
+                rhop[i-t0][:] = np.histogram(np.concatenate(
                                                 f.rhop/np.mean(f.rhop)),
                                             bins = bins)[0]
             else:
-                rhop[t0-i][:] = np.histogram(
+                rhop[i-t0][:] = np.histogram(
                                             np.concatenate(np.concatenate(
                                                 f.rhop/np.mean(f.rhop))),
                                             bins = bins)[0]
-        avgrhop = np.mean(rhop, axis=0)
-        print(rhop.shape)
-        print(avgrhop)
+        avgrhop[run] = np.mean(rhop, axis=0)
+        stdrhop[run] = np.std(rhop, axis=0)
+
+        sys.stdout = open(os.devnull, 'w')
         param = pc.read.parameters(datadir = datadir + run + '/data/')
-        plt.step(bins[:-1], avgrhop,
+        sys.stdout = sys.__stdout__
+        plt.step(bins[:-1], np.mean(rhop, axis=0),
             label = r'$\tau_s = {},\ \epsilon = {}$'.format(
                 param.taus, param.eps_dtog))
 
@@ -163,8 +270,34 @@ def time_averaged_density_histogram(t0, t,
 
     # Save the plot
     if type(filename) is str:
-        plt.savefig(plotdir + filename + '.pdf', bbox_inches='tight')
-
+            plt.savefig(plotdir + filename + '.pdf', bbox_inches='tight')
     plt.show()
 
+    # Create plot for standard deviations.
+    # Set up axis
+    if xlog and ylog:
+        plt.xscale('log')
+        plt.yscale('log')
+    elif xlog:
+        plt.xscale('log')
+    elif ylog:
+        plt.yscale('log')
+    if type(xlim) is tuple:
+        plt.xlim(xlim)
 
+    for run in runs:
+        sys.stdout = open(os.devnull, 'w')
+        param = pc.read.parameters(datadir = datadir + run + '/data/')
+        sys.stdout = sys.__stdout__
+        plt.step(bins[:-1], stdrhop[run]/avgrhop[run],
+                label = r'$\tau_s = {},\ \epsilon = {}$'.format(
+                param.taus, param.eps_dtog))
+    plt.minorticks_on()
+    plt.xlabel(r"$\rho_p/<\rho_p>$")
+    plt.ylabel(r"Relative standard deviation, $\sigma_{\rho_p}/N(\rho_p)$")
+    plt.legend(loc='best')
+
+    # Save the plot
+    if type(filename) is str:
+            plt.savefig(plotdir + filename + '_std.pdf', bbox_inches='tight')
+    plt.show()
