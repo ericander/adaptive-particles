5f6c6922d88866cbc4e02c85ac90204f58461ded
Eric Andersson <eaherrgarden@gmail.com>
2017-07-11 11:03:24 +0200

    Added function for making denisty histogram for data in 3D. This function is not yet tested.

--- animate.py
+++ animate.py
@@ -284,7 +284,7 @@ def rhop_histogram_2D(animdir = './animations/', xlim = (0.01, 10),
     ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
     if ndim == 3:
         raise NotImplementedError(
-                "For 3D use animate.rhop_histogram_3D")
+                "Detected 3D. For 3D use animate.rhop_histogram_3D")
 
     # Import and set-up data from the pencil-code
     print("Reading and setting up data")
@@ -379,6 +379,151 @@ def rhop_histogram_2D(animdir = './animations/', xlim = (0.01, 10),
 
     anim = animation.FuncAnimation(fig, animate, init_func=init,
                                 frames=t.size, interval=200,
+                                blit=False, repeat = False)
+    anim.save(animdir + filename + '.mp4', fps = fps,
+            extra_args=['-vcodec', 'libx264'])
+    print("Done.")
+    if show_off:
+        plt.clear()
+    else:
+        plt.show()
+
+#======================================================================
+
+def rhop_histogram_3D(tmax, animdir = './animations/',
+        xlim = (0.01, 10), ylim = (0, 600), xlog = False, ylog = False,
+        filename = 'animation', fps = 24, show_off = False, dsnap = 1):
+    """ Plots the cumulative particle density distribution.
+
+    Keyword Argument:
+        animdir
+            Directory for saving animation.
+        xlim
+            If limits is of type tuple it will set the x-axis limit
+            to the given values.
+        ylim
+            If limits is of type tuple it will set the y-axis limit
+            to the given values.
+        xlog
+            True/False whether to use log scale on x-axis or not.
+        ylog
+            True/False whether to use log scale on y-axis or not.
+        filename
+            Name of output file.
+        fps
+            Frames per second in animation
+        show_off
+            If true, turns off playing the animation after finilizing
+        dsnap
+            Time between each time stamp.
+    """
+    # Eric Andersson, 06-07-2017
+    import PencilCode as pc
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+    import sys
+    import os
+
+    print('Iniziating ...')
+    # Check dimensions
+    dim = pc.read.dimensions()
+    ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
+    if ndim != 3:
+        print('Could not detect 3 dimensions. Will run animate.rhop_histogram_2D instead for better cadence.')
+        rhop_histogram_2D(animdir = animdir, xlim = xlim,
+        ylim = ylim, xlog = xlog, ylog = ylog,
+        filename = filename, fps = fps, show_off = show_off)
+        exit()
+
+    # Parameters used in run
+    print("Setting up parameters...")
+    param = pc.read.parameters()
+    taus = param.taus               # Dimensionless stopping time
+    eps = param.eps_dtog            # Local mass density ratio
+    print("Done.")
+
+    # Set up figure
+    print("Setting up figure...")
+    fig  = plt.figure()
+    ax = fig.add_subplot(111)
+    print("Done.")
+
+    # Set up axis
+    if ylog:
+        ax.set_yscale('log')
+    if type(xlim) is tuple:
+        if xlog:
+            bins = np.logspace(np.log10(xlim[0]),
+                                    np.log10(xlim[1]), 50)
+        else:
+            bins = np.linspace(xlim[0], xlim[1], 50)
+    else:
+        bins = 'auto'
+
+    # Function that sets up background of each frame
+    def init():
+        ax.text(0.75, 0.85, r'$\tau_s = {},\ \epsilon = {}$'.format(
+                taus, eps), transform=ax.transAxes)
+        plt.minorticks_on()
+        plt.xlabel(r'$\rho_p/<\rho_p>$')
+        plt.ylabel(r'$N_p$')
+        if type(ylim) is tuple:
+            plt.ylim(ylim)
+        # Set axis scales
+        if xlog and ylog:
+            plt.xscale('log')
+            plt.yscale('log')
+        elif xlog:
+            plt.xscale('log')
+        elif ylog:
+            plt.yscale('log')
+        # Set up time text
+        time_text = ax.text(0.75, 0.95,
+            r'Time = {0:.2f} $\Omega t$'.format(t[0]),
+            transform=ax.transAxes)
+        sys.stdout = open(os.devnull, 'w')
+        rhop = pc.read.var('rhop', ivar = 0)
+        sys.stdout = sys.__stdout__
+        ax.hist(np.concatenate(np.concatenate(
+                    rhop))/np.mean(rhop),
+                bins = bins)
+
+    # Function that is called everytime a new frame is produced
+    def animate(i):
+        print("\rAnimating ({:6.1%})......".format(i/(tmax/dsnap)),
+                end='', flush=True)
+        #Read data
+        sys.stdout = open(os.devnull, 'w')
+        f = pc.read.var(ivar = i)
+        sys.stdout = sys.__stdout__
+        # Reset figure.
+        ax.clear()
+        plt.minorticks_on()
+        plt.xlabel(r'$\rho_p/<\rho_p>$')
+        plt.ylabel(r'$N_{\rm cells}$')
+        if type(ylim) is tuple:
+            plt.ylim(ylim)
+        # Set axis scales
+        if xlog and ylog:
+            plt.xscale('log')
+            plt.yscale('log')
+        elif xlog:
+            plt.xscale('log')
+        elif ylog:
+            plt.yscale('log')
+        # Plot data
+        ax.text(0.75, 0.85, r'$\tau_s = {},\ \epsilon = {}$'.format(
+                taus, eps), transform=ax.transAxes)
+        ax.text(0.75, 0.95,
+            r'Time = {0:.2f} $\Omega t$'.format(t[i]),
+            transform=ax.transAxes)
+        ax.hist(np.concatenate(
+            np.concatenate(f.rhop))/np.mean(rhop),
+                bins = bins)
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                frames=int(tmax/dsnap), interval=200,
                                 blit=False, repeat = False)
     anim.save(animdir + filename + '.mp4', fps = fps,
             extra_args=['-vcodec', 'libx264'])
